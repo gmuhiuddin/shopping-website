8ba5a45bc877486632158c211c22a1f97c1b32a3
Ghulam_muhiuddin <muhiuddinghulam825@gmail.com>
2023-11-11 16:20:27 +0500

    added some changes

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const products = [
     title: 'SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s',
     price: 109,
     description:
-      'Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)',
+      'Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical ',
     category: 'electronics',
     image: 'https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg',
     rating: { rate: 2.9, count: 470 }
@@ -107,7 +107,7 @@ const products = [
       'Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5',
     price: 109,
     description:
-      '3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.',
+      '3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks.',
     category: 'electronics',
     image: 'https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg',
     rating: { rate: 4.8, count: 319 }
@@ -421,15 +421,7 @@ let productNameByUser = document.getElementById('product-name-by-user');
 let check = document.getElementById('check');
 let logo = document.getElementById('logo');
 
-logo.addEventListener('click', searchItemByUser)
-logo.addEventListener('click', cartsLoop)
 logo.addEventListener('click', () => {
-  poppup[0].style.display = 'none'
-  HToL.checked = false;
-  LTOH.checked = false;
-  all.checked = true;
-  above3.checked = false;
-  above4.checked = false;
   window.location.reload();
 
 })
@@ -470,7 +462,9 @@ function showCarts(product) {
 <h3 class="product-title">${product.title}</h3>
 <b class="product-price">${product.price} $</b>
 <h5 class="product-rating"><i class="fas fa-star" style="color: #fbff05;"></i> ${product.rating.rate} (${product.rating.count})</h5>
-  </div>`
+<button class='addToCartBtn'>Add to Cart</button>  
+<button class='buyNowBtn'>Buy now</button>  
+</div>`
   container[0].innerHTML += cartsHtml;
 
 };
